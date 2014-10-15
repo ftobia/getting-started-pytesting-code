7ccb4f1bffb62f496beb0cb236cce0176d0efb78
ftobia <frank.tobia@gmail.com>
2014-10-15 07:59:04 -0400

    Need to install package for it to be on PYTHONPATH by default.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='my_cool_library',
+    version='0.1',
+    description='Sample code to try out pytest',
+    author='Frank Tobia',
+    author_email='frank.tobia@gmail.com',
+    url='https://github.com/ftobia/getting-started-pytesting-code.git',
+    py_modules=['my_cool_library'],
+)
+
